Fix units and amplitude forcing.

calibration_graph/04_resonator_spectroscopy_multiplexed.py
@@ -23,8 +23,8 @@ from typing import Optional
 class Parameters(NodeParameters):
     qubits: Optional[str] = None
     num_averages: int = 100
-    frequency_span: int = 15_000_000
-    frequency_step: int = 100_000
+    frequency_span_in_MHz: float = 15
+    frequency_step_in_MHz: int = 0.1
     simulate: bool = False
 
 node = QualibrationNode(
@@ -88,12 +88,12 @@ num_resonators = len(resonators)
 live_plot = True
 n_avg = node.parameters.num_averages  # The number of averages
 # The frequency sweep around the resonator resonance frequency f_opt
-span, step = node.parameters.frequency_span, node.parameters.frequency_step
+span = node.parameters.frequency_span * u.MHz
+step = node.parameters.frequency_step * u.MHz
 dfs = np.arange(-span/2, +span/2, step)
 # You can adjust the IF frequency here to manually adjust the resonator frequencies instead of updating the state
 # rr1.intermediate_frequency = -50 * u.MHz
 # rr2.intermediate_frequency = 50 * u.MHz
-
 
 with program() as multi_res_spec:
     # Declare 'I' and 'Q' and the corresponding streams for the two resonators.

calibration_graph/05a_resonator_spectroscopy_vs_amplitude.py
@@ -27,8 +27,8 @@ from typing import Optional
 class Parameters(NodeParameters):
     qubits: Optional[str] = None
     num_averages: int = 100
-    frequency_span: int = 10_000_000
-    frequency_step: int = 100_000
+    frequency_span_in_MHz: float = 10
+    frequency_step_in_MHz: float = 0.1
     simulate: bool = False
 
 node = QualibrationNode(
@@ -96,16 +96,24 @@ n_avg = node.parameters.num_averages  # The number of averages
 # for rr in resonators:
 #     rr.operations["readout"].amplitude = 0.25
 
-for q in qubits:
-    q.resonator.operations["readout"].amplitude = 0.4
+tracked_qubits = []
+for qubit in qubits:
+    with tracked_updates(qubit, auto_revert=False, dont_assign_to_none=True) as qubit:
+        qubit.resonator.operations["readout"].amplitude = 0.4
+        tracked_qubits.append(qubit)
+
 config = machine.generate_config()
-    
+for tracked_qubit in tracked_qubits:
+    tracked_qubit.revert_changes()
+
 # The readout amplitude sweep (as a pre-factor of the readout amplitude) - must be within [-2; 2)
 # amps = np.arange(0.05, 1.00, 0.02)
 amps = np.logspace(-2, 0.0, 100)
 # The frequency sweep around the resonator resonance frequencies f_opt
-span = node.parameters.frequency_span
-dfs = np.arange(-span/2, +span/2, 0.05e6)
+span = node.parameters.frequency_span * u.MHz
+step = node.parameters.frequency_step * u.MHz
+dfs = np.arange(-span//2, +span//2, step)
+
 
 with program() as multi_res_spec_vs_amp:
     # Declare 'I' and 'Q' and the corresponding streams for the two resonators.

calibration_graph/05b_resonator_spectroscopy_vs_flux.py
@@ -26,11 +26,11 @@ from typing import Optional, Literal
 class Parameters(NodeParameters):
     qubits: Optional[str] = None
     num_averages: int = 20
-    min_flux_offset: float = -0.5
-    max_flux_offset: float = 0.5
+    min_flux_offset_in_V: float = -0.5
+    max_flux_offset_in_V: float = 0.5
     num_flux_points: int = 201
-    frequency_span: int = 10_000_000
-    frequency_step: int = 50_000
+    frequency_span_in_MHz: float = 10
+    frequency_step_in_MHz: float = 0.05
     flux_point_joint_or_independent: Literal['joint', 'independent'] = "joint"
     simulate: bool = False
 
@@ -96,8 +96,9 @@ dcs = np.linspace(node.parameters.min_flux_offset,
                   node.parameters.max_flux_offset,
                   node.parameters.num_flux_points)
 # The frequency sweep around the resonator resonance frequency f_opt
-span = node.parameters.frequency_span
-dfs = np.arange(-span/2, +span/2, node.parameters.frequency_step)
+span = node.parameters.frequency_span * u.MHz
+step = node.parameters.frequency_step * u.MHz
+dfs = np.arange(-span/2, +span/2, step)
 
 flux_point = node.parameters.flux_point_joint_or_independent  # 'independent' or 'joint'
 update_flux_min = True  # Update the min flux point

calibration_graph/06_qubit_spectroscopy_sequential.py
@@ -36,8 +36,8 @@ class Parameters(NodeParameters):
     operation: str = "saturation"
     operation_amplitude_factor: Optional[float] = None
     operation_len: Optional[int] = None
-    frequency_span: int = 10_000_000
-    frequency_step: int = 50_000
+    frequency_span_in_MHz: float = 10
+    frequency_step_in_MHz: float = 0.05
     flux_point_joint_or_independent: Literal['joint', 'independent'] = "joint"
     target_peak_width: Optional[int] = None
     simulate: bool = False
@@ -57,13 +57,11 @@ from qualang_tools.plot import interrupt_on_close
 from qualang_tools.loops import from_array
 from qualang_tools.units import unit
 from quam_libs.components import QuAM
-from quam_libs.macros import qua_declaration, multiplexed_readout, node_save
+from quam_libs.macros import qua_declaration
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-import matplotlib
-import xarray as xr
 from lib.plot_utils import QubitGrid, grid_iter
 from lib.save_utils import fetch_results_as_xarray
 
@@ -100,12 +98,14 @@ n_avg = node.parameters.num_averages  # The number of averages
 # Adjust the pulse duration and amplitude to drive the qubit into a mixed state
 operation_len = node.parameters.operation_len  # can be None - will just be ignored
 if node.parameters.operation_amplitude_factor:
-    operation_amp = node.parameters.operation_amp  # pre-factor to the value defined in the config - restricted to [-2; 2)
+    # pre-factor to the value defined in the config - restricted to [-2; 2)
+    operation_amp = node.parameters.operation_amplitude_factor
 else:
-    operation_amp = 1
+    operation_amp = 1.
 # Qubit detuning sweep with respect to their resonance frequencies
-span = node.parameters.frequency_span
-dfs = np.arange(-span/2, +span/2, node.parameters.frequency_step)
+span = node.parameters.frequency_span * u.MHz
+step = node.parameters.frequency_step * u.MHz
+dfs = np.arange(-span//2, +span//2, step)
 flux_point = node.parameters.flux_point_joint_or_independent  # 'independent' or 'joint'
 
 target_peak_width = node.parameters.target_peak_width