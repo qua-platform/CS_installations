Fix units and amplitude forcing.

calibration_graph/05a_resonator_spectroscopy_vs_amplitude.py
@@ -27,8 +27,8 @@ from typing import Optional
 class Parameters(NodeParameters):
     qubits: Optional[str] = None
     num_averages: int = 100
-    frequency_span_in_MHz: float = 10
-    frequency_step_in_MHz: float = 0.1
+    frequency_span_in_mhz: float = 10
+    frequency_step_in_mhz: float = 0.1
     simulate: bool = False
 
 node = QualibrationNode(
@@ -110,8 +110,8 @@ for tracked_qubit in tracked_qubits:
 # amps = np.arange(0.05, 1.00, 0.02)
 amps = np.logspace(-2, 0.0, 100)
 # The frequency sweep around the resonator resonance frequencies f_opt
-span = node.parameters.frequency_span * u.MHz
-step = node.parameters.frequency_step * u.MHz
+span = node.parameters.frequency_span_in_mhz * u.MHz
+step = node.parameters.frequency_step_in_mhz * u.MHz
 dfs = np.arange(-span//2, +span//2, step)
 
 

calibration_graph/05b_resonator_spectroscopy_vs_flux.py
@@ -26,11 +26,11 @@ from typing import Optional, Literal
 class Parameters(NodeParameters):
     qubits: Optional[str] = None
     num_averages: int = 20
-    min_flux_offset_in_V: float = -0.5
-    max_flux_offset_in_V: float = 0.5
+    min_flux_offset_in_v: float = -0.5
+    max_flux_offset_in_v: float = 0.5
     num_flux_points: int = 201
-    frequency_span_in_MHz: float = 10
-    frequency_step_in_MHz: float = 0.05
+    frequency_span_in_mhz: float = 10
+    frequency_step_in_mhz: float = 0.05
     flux_point_joint_or_independent: Literal['joint', 'independent'] = "joint"
     simulate: bool = False
 
@@ -92,12 +92,12 @@ num_resonators = len(resonators)
 
 n_avg = node.parameters.num_averages  # The number of averages
 # Flux bias sweep in V
-dcs = np.linspace(node.parameters.min_flux_offset,
-                  node.parameters.max_flux_offset,
+dcs = np.linspace(node.parameters.min_flux_offset_in_v,
+                  node.parameters.max_flux_offset_in_v,
                   node.parameters.num_flux_points)
 # The frequency sweep around the resonator resonance frequency f_opt
-span = node.parameters.frequency_span * u.MHz
-step = node.parameters.frequency_step * u.MHz
+span = node.parameters.frequency_span_in_mhz * u.MHz
+step = node.parameters.frequency_step_in_mhz * u.MHz
 dfs = np.arange(-span/2, +span/2, step)
 
 flux_point = node.parameters.flux_point_joint_or_independent  # 'independent' or 'joint'

calibration_graph/06_qubit_spectroscopy_sequential.py
@@ -36,8 +36,8 @@ class Parameters(NodeParameters):
     operation: str = "saturation"
     operation_amplitude_factor: Optional[float] = None
     operation_len: Optional[int] = None
-    frequency_span_in_MHz: float = 10
-    frequency_step_in_MHz: float = 0.05
+    frequency_span_in_mhz: float = 10
+    frequency_step_in_mhz: float = 0.05
     flux_point_joint_or_independent: Literal['joint', 'independent'] = "joint"
     target_peak_width: Optional[int] = None
     simulate: bool = False
@@ -103,9 +103,9 @@ if node.parameters.operation_amplitude_factor:
 else:
     operation_amp = 1.
 # Qubit detuning sweep with respect to their resonance frequencies
-span = node.parameters.frequency_span * u.MHz
-step = node.parameters.frequency_step * u.MHz
-dfs = np.arange(-span//2, +span//2, step)
+span = node.parameters.frequency_span_in_mhz * u.MHz
+step = node.parameters.frequency_step_in_mhz * u.MHz
+dfs = np.arange(-span//2, +span//2, step, dtype=np.int32)
 flux_point = node.parameters.flux_point_joint_or_independent  # 'independent' or 'joint'
 
 target_peak_width = node.parameters.target_peak_width
@@ -283,7 +283,7 @@ if not simulate:
         def foo(freq):
             return freq + q.xy.intermediate_frequency + q.xy.LO_frequency
         return foo
-    ds = ds.assign_coords({'freq_full' : (['qubit','freq'],np.array([abs_freq(q)(dfs) for q in qubits]))})
+    ds = ds.assign_coords({'freq_full' : (['qubit','freq'],np.array([abs_freq(q)(dfs) for q in qubits], dtype=np.int32))})
     ds = ds.assign({'phase': np.arctan2(ds.Q,ds.I)})
 
     ds.freq_full.attrs['long_name'] = 'Frequency'