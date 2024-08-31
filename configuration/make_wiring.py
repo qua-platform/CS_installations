09ec27dff9f073709575832f259645a401d58394
Dean Poulos <dean.poulos7@gmail.com>
2024-08-31 14:04:22 +1000

    Small refactor.

--- configuration/make_wiring.py
+++ configuration/make_wiring.py
@@ -13,23 +13,23 @@ path = Path(".") / "quam_state"
 
 # 2. Define the available instrument setup
 instruments = Instruments()
-# instruments.add_lf_fem(controller=1, slots=2)
-# instruments.add_mw_fem(controller=1, slots=1)
 instruments.add_opx_plus(controllers = [1])
 instruments.add_octave(indices = 1)
+# instruments.add_lf_fem(controller=1, slots=2)
+# instruments.add_mw_fem(controller=1, slots=1)
 
 # Define any custom/hardcoded channel addresses
 q1_drive_ch = None  # will resort to automatic allocation
 # q1_drive_ch = opx_iq_octave_spec(out_port_i=9, out_port_q=10, rf_out=5)
 
 # Define which quantum elements are present in the system
-connectivity = Connectivity()
 qubits = [1]
-# connectivity.add_qubit_pair_flux_lines(qubit_pairs=[(1,2)])
+connectivity = Connectivity()
 connectivity.add_resonator_line(qubits=qubits, triggered=True)
 connectivity.add_qubit_flux_lines(qubits=qubits)
 connectivity.add_qubit_drive_lines(qubits=qubits[0], triggered=True, constraints=q1_drive_ch)
 # connectivity.add_qubit_drive_lines(qubits=qubits[1:], triggered=True)
+# connectivity.add_qubit_pair_flux_lines(qubit_pairs=[(1,2)])
 
 # Allocate the wiring to the connectivity object based on the available instruments
 allocate_wiring(connectivity, instruments)
