22751909954d5a71bd5f5f429a437e3e74bbb350
Dean Poulos <dean.poulos7@gmail.com>
2024-08-31 14:03:18 +1000

    Add quam modifciation convenience script.

--- configuration/make_quam.py
+++ configuration/make_quam.py
@@ -7,12 +7,13 @@ path = Path(".") / "quam_state"
 
 machine = QuAM.load(path)
 
-# Configure the octave
-# If "externally" configured, ip is default (host) port is 11XXX where XXX are the last three digits of octave IP
-# octave_settings = {"oct1": {"port": 11234} }
-# If "internally" configured, port is default (80) ip is given by the local ip address of the Octave
+# If "externally" configured, octave ip is default (host) port is 11XXX where XXX are the last three digits of octave IP
+octave_settings = {"oct1": {"port": 11235} }
+
+# If "internally" configured, octave port is default (80) ip is given by the local ip address of the Octave
 # octave_settings = {"oct1": {"ip": "192.168.88.250"} }
-octave_settings = {}
+
+# octave_settings = {}
 
 # Make the QuAM object and save it
 quam = build_quam(machine, quam_state_path=path, octaves_settings=octave_settings)
--- configuration/make_wiring.py
+++ configuration/make_wiring.py
@@ -1,38 +1,41 @@
 from pathlib import Path
 
+from qualang_tools.wirer.wirer.channel_specs import *
 from qualang_tools.wirer import Instruments, Connectivity, allocate_wiring, visualize
 from quam_libs.quam_builder.machine import build_quam_wiring
-from qualang_tools.wirer.wirer.channel_specs import *
 
-# 1. Define static parameters
-host_ip = "172.16.33.107"  # "172.16.33.101"
-cluster_name = "Cluster_1"
+# Define static parameters
+host_ip = "10.2.0.201"
+cluster_name = "DGX_Cluster"
+# host_ip = "172.16.33.107"  # "172.16.33.101"
+# cluster_name = "Beta_8"  # "Cluster_81"
 path = Path(".") / "quam_state"
 
 # 2. Define the available instrument setup
 instruments = Instruments()
-instruments.add_lf_fem(controller=1, slots=2)
-instruments.add_mw_fem(controller=1, slots=1)
-# instruments.add_opx_plus(controllers = [1])
-# instruments.add_octave(indices = 1)
+# instruments.add_lf_fem(controller=1, slots=2)
+# instruments.add_mw_fem(controller=1, slots=1)
+instruments.add_opx_plus(controllers = [1])
+instruments.add_octave(indices = 1)
 
-# 3a. Define any custom/hardcoded channel addresses
+# Define any custom/hardcoded channel addresses
 q1_drive_ch = None  # will resort to automatic allocation
 # q1_drive_ch = opx_iq_octave_spec(out_port_i=9, out_port_q=10, rf_out=5)
 
-# 3b. Define which quantum elements are present in the system
+# Define which quantum elements are present in the system
 connectivity = Connectivity()
+qubits = [1]
 # connectivity.add_qubit_pair_flux_lines(qubit_pairs=[(1,2)])
-connectivity.add_resonator_line(qubits=[1, 2], triggered=True)
-connectivity.add_qubit_flux_lines(qubits=[1, 2])
-connectivity.add_qubit_drive_lines(qubits=[1], triggered=True, constraints=q1_drive_ch)
-connectivity.add_qubit_drive_lines(qubits=[2], triggered=True)
+connectivity.add_resonator_line(qubits=qubits, triggered=True)
+connectivity.add_qubit_flux_lines(qubits=qubits)
+connectivity.add_qubit_drive_lines(qubits=qubits[0], triggered=True, constraints=q1_drive_ch)
+# connectivity.add_qubit_drive_lines(qubits=qubits[1:], triggered=True)
 
-# 3c. View wiring schematic
-# visualize(connectivity.elements, available_channels=instruments.available_channels)
-
-# 4. Allocate the wiring to the connectivity object based on the available instruments
+# Allocate the wiring to the connectivity object based on the available instruments
 allocate_wiring(connectivity, instruments)
+
+# View wiring schematic
+visualize(connectivity.elements, available_channels=instruments.available_channels)
 
 # Build the wiring and network into a QuAM machine and save it as "wiring.json"
 build_quam_wiring(connectivity, host_ip, cluster_name, path)
