d59cbfd12f4ccc34a33c512a182e6487ad7f7dfc
Kevin <kevin@quantum-machines.co>
2024-09-05 14:38:21 -0400

    [modify_quam] -> now can choose sampling_rate, up_sampling_mode, and output_mode

--- configuration/modify_quam.py
+++ configuration/modify_quam.py
@@ -14,6 +14,12 @@ u = unit(coerce_to_integer=True)
 # Change active qubits
 # machine.active_qubit_names = ["q0"]
 
+for _ in machine.ports.analog_outputs.con1:
+    for r in machine.ports.analog_outputs.con1[_]:
+            machine.ports.analog_outputs.con1[_][r].sampling_rate = 1e9
+            machine.ports.analog_outputs.con1[_][r].upsampling_mode = 'pulse'
+            machine.ports.analog_outputs.con1[_][r].output_mode = "amplified"
+
 save_machine(machine, path)
 
 # View the corresponding "raw-QUA" config
