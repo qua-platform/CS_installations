e6b353911186e55543b277ab819a09c6b66d9e19
Kevin <kevin@quantum-machines.co>
2024-09-06 21:51:06 -0400

    moving into OPX1k

--- calibration_graph/hello_qua.py
+++ calibration_graph/hello_qua.py
@@ -1,25 +1,4 @@
 #%%
-from qualibrate import QualibrationNode, NodeParameters
-from typing import Optional, Literal
-
-class Parameters(NodeParameters):
-    qubits: Optional[str] = None
-    num_averages: int = 200
-    operation: str = "x180"
-    min_amp_factor: float = 0.00001
-    max_amp_factor: float = 2.0
-    amp_factor_step: float = 0.005
-    max_number_rabi_pulses_per_sweep: int = 1
-    flux_point_joint_or_independent: Literal['joint', 'independent'] = "joint"
-    simulate: bool = False
-
-node = QualibrationNode(
-    name="fooo",
-    parameters_class=Parameters
-)
-
-node.parameters = Parameters()
-
 from qm.qua import *
 from qm import SimulationConfig
 from qualang_tools.units import unit
@@ -44,12 +23,7 @@ octave_config = machine.get_octave_config()
 # Open Communication with the QOP
 qmm = machine.connect()
 
-# Get the relevant QuAM components
-if node.parameters.qubits is None or node.parameters.qubits == '':
-    qubits = machine.active_qubits
-else:
-    qubits = [machine.qubits[q] for q in node.parameters.qubits.replace(' ', '').split(',')]
-num_qubits = len(qubits)
+qubits = machine.active_qubits
 
 with program() as prog:
 
@@ -61,15 +35,15 @@ with program() as prog:
 
     with infinite_loop_():
 
-        with for_(a, 0, a < 1.0, a +0.1):
+        # with for_(a, 0, a < 1.0, a +0.1):
 
-            qubits[2].xy.play('x180', amplitude_scale=a)
-            qubits[1].xy.play('x180', amplitude_scale=a)
-            wait(4)
-            align()
-            qubits[0].resonator.play('const')
-            align()
-            wait(4)
+        qubits[2].xy.play('x180')
+        # qubits[1].xy.play('x180')
+        # wait(4)
+        # align()
+        qubits[0].resonator.play('const')
+        # align()
+        wait(1_000)
         # for qubit in machine.active_qubits:
         #     qubit.z.play('const')
         #     qubit.z.wait(4)
