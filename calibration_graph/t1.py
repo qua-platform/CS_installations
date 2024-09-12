d09d8233d3a0a941b3504e5fa3c4584ce87ca62b
TomDvirQM <tom.dvir@quantum-machines.co>
2024-09-12 11:47:50 +0200

    fixed analysis for T1

--- calibration_graph/t1.py
+++ calibration_graph/t1.py
@@ -1,15 +1,18 @@
+# %%
 from qualibrate import QualibrationNode, NodeParameters
 from typing import Optional, Literal
 
 
 class Parameters(NodeParameters):
-    qubits: Optional[str] = 'q5'
+    qubits: Optional[str] = None
     num_averages: int = 100
     min_wait_time_in_ns: int = 16
-    max_wait_time_in_ns: int = 4000
-    wait_time_step_in_ns: int = 20
+    max_wait_time_in_ns: int = 40000
+    wait_time_step_in_ns: int = 200
     flux_point_joint_or_independent: Literal['joint', 'independent'] = "joint"
     simulate: bool = False
+    use_state_discrimination: bool = True
+    reset_type: Literal['active', 'thermal'] = "thermal"
 
 node = QualibrationNode(
     name="t1_experiment",
@@ -26,7 +29,7 @@ from qualang_tools.plot import interrupt_on_close
 from qualang_tools.loops import from_array, get_equivalent_log_array
 from qualang_tools.units import unit
 from quam_libs.components import QuAM
-from quam_libs.macros import qua_declaration, multiplexed_readout, node_save
+from quam_libs.macros import qua_declaration, multiplexed_readout, node_save, active_reset, readout_state
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -34,7 +37,7 @@ import numpy as np
 import matplotlib
 from quam_libs.lib.plot_utils import QubitGrid, grid_iter
 from quam_libs.lib.save_utils import fetch_results_as_xarray
-from quam_libs.lib.fit import fit_oscillation_decay_exp, oscillation_decay_exp
+from quam_libs.lib.fit import fit_decay_exp, decay_exp
 
 # matplotlib.use("TKAgg")
 
@@ -58,7 +61,7 @@ if node.parameters.qubits is None or node.parameters.qubits == '':
 else:
     qubits = [machine.qubits[q] for q in node.parameters.qubits.replace(' ', '').split(',')]
 num_qubits = len(qubits)
-# %%
+
 ###################
 # The QUA program #
 ###################
@@ -76,9 +79,9 @@ flux_point = node.parameters.flux_point_joint_or_independent  # 'independent' or
 with program() as t1:
     I, I_st, Q, Q_st, n, n_st = qua_declaration(num_qubits=num_qubits)
     t = declare(int)  # QUA variable for the idle time
-    sign = declare(int)  # QUA variable to change the sign of the detuning
-    phi = declare(fixed)  # QUA variable for dephasing the second pi/2 pulse (virtual Z-rotation)
-
+    if node.parameters.use_state_discrimination:
+        state = [declare(int) for _ in range(num_qubits)]
+        state_st = [declare_stream() for _ in range(num_qubits)]
     for i, q in enumerate(qubits):
 
         # Bring the active qubits to the minimum frequency point
@@ -98,7 +101,12 @@ with program() as t1:
         with for_(n, 0, n < n_avg, n + 1):
             save(n, n_st)
             with for_(*from_array(t, idle_times)):
-
+                if node.parameters.reset_type == "active":
+                    active_reset(machine, q.name)
+                else:
+                    q.resonator.wait(machine.thermalization_time * u.ns)
+                    q.align()
+                    
                 q.xy.play("x180")
 
                 q.xy.wait(t)
@@ -107,22 +115,25 @@ with program() as t1:
                 align()
 
                 # Measure the state of the resonators
-                q.resonator.measure("readout", qua_vars=(I[i], Q[i]))
-
-                # save data
-                save(I[i], I_st[i])
-                save(Q[i], Q_st[i])
-
-                # Wait for the qubits to decay to the ground state
-                q.resonator.wait(machine.thermalization_time * u.ns)
+                if node.parameters.use_state_discrimination:
+                    readout_state(q, state[i])
+                    save(state[i], state_st[i])
+                else:
+                    q.resonator.measure("readout", qua_vars=(I[i], Q[i]))
+                    # save data
+                    save(I[i], I_st[i])
+                    save(Q[i], Q_st[i])
 
         align()
 
     with stream_processing():
         n_st.save("n")
         for i in range(num_qubits):
-            I_st[i].buffer(len(idle_times)).average().save(f"I{i + 1}")
-            Q_st[i].buffer(len(idle_times)).average().save(f"Q{i + 1}")
+            if node.parameters.use_state_discrimination:
+                state_st[i].buffer(len(idle_times)).average().save(f"state{i + 1}")
+            else:
+                I_st[i].buffer(len(idle_times)).average().save(f"I{i + 1}")
+                Q_st[i].buffer(len(idle_times)).average().save(f"Q{i + 1}")
 
 ###########################
 # Run or Simulate Program #
@@ -148,7 +159,7 @@ else:
     # Get results from QUA program
     for i in range(num_qubits):
         print(f"Fetching results for qubit {qubits[i].name}")
-        data_list = sum([[f"I{i + 1}", f"Q{i + 1}"] ], ["n"])
+        data_list = ["n"]
         results = fetching_tool(job, data_list, mode="live")
     # Live plotting
     # fig, axes = plt.subplots(2, num_qubits, figsize=(4 * num_qubits, 8))
@@ -162,34 +173,68 @@ else:
 
     qm.close()
 
-    for i in range(num_qubits):
-        print(f"Fetching results for qubit {qubits[i].name}")
-        data_list = sum([[f"I{i + 1}", f"Q{i + 1}"] ], ["n"])
-        results = fetching_tool(job, data_list)
 
-    data = results.fetch_all()
+# %%
+if not simulate:
+    handles = job.result_handles
+    ds = fetch_results_as_xarray(handles, qubits, {"idle_time": idle_times})
 
-    fig = plt.figure()
-    plt.plot(idle_times * 4, data[1])
+    ds = ds.assign_coords(idle_time=4*ds.idle_time/1e3)  # convert to usec
+    ds.idle_time.attrs = {'long_name': 'idle time', 'units': 'usec'}
+
+# %%
+if not simulate:
+    if node.parameters.use_state_discrimination:
+        fit_data = fit_decay_exp(ds.state, 'idle_time')
+    else:
+        fit_data = fit_decay_exp(ds.I, 'idle_time')
+    fit_data.attrs = {'long_name' : 'time', 'units' : 'usec'}
+    fitted =  decay_exp(ds.idle_time,
+                                                    fit_data.sel(
+                                                        fit_vals="a"),
+                                                    fit_data.sel(
+                                                        fit_vals="offset"),
+                                                    fit_data.sel(fit_vals="decay"))
+
+
+    decay = fit_data.sel(fit_vals = 'decay')
+    decay.attrs = {'long_name' : 'decay', 'units' : 'nSec'}
+
+    decay_res = fit_data.sel(fit_vals = 'decay_decay')
+    decay_res.attrs = {'long_name' : 'decay', 'units' : 'nSec'}
+    
+    tau = -1/fit_data.sel(fit_vals='decay')
+    tau.attrs = {'long_name' : 'T2*', 'units' : 'uSec'}
+
+    tau_error = -tau * (np.sqrt(decay_res)/decay)
+    tau_error.attrs = {'long_name' : 'T2* error', 'units' : 'uSec'}
+
+node.results = {}
+node.results['ds'] = ds
+# %%
+if not simulate:
+    grid_names = [f'{q.name}_0' for q in qubits]
+    grid = QubitGrid(ds, grid_names)
+    for ax, qubit in grid_iter(grid):
+        if node.parameters.use_state_discrimination:
+            ds.sel(qubit = qubit['qubit']).state.plot(ax = ax)
+            
+            ax.set_ylabel('State')
+        else:
+            ds.sel(qubit = qubit['qubit']).I.plot(ax = ax)
+            ax.set_ylabel('I (V)')
+        ax.plot(ds.idle_time, fitted.loc[qubit], 'r--')
+        ax.set_title(qubit['qubit'])
+        ax.set_xlabel('Idle_time (uS)')
+        ax.text(0.1, 0.9, f'T1 = {tau.sel(qubit = qubit["qubit"]).values:.1f} + {tau_error.sel(qubit = qubit["qubit"]).values:.1f} usec', transform=ax.transAxes, fontsize=10,
+        verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5))
+    grid.fig.suptitle('T1')
+    plt.tight_layout()
     plt.show()
+    node.results['figure_raw'] = grid.fig
 
-    # Fit the results to extract the qubit decay time T1
-    try:
-        from qualang_tools.plot.fitting import Fit
-
-        fit = Fit()
-        fig2 = plt.figure()
-        decay_fit = fit.T1(4 * idle_times, data[1], plot=True)
-        qubit_T1 = np.round(np.abs(decay_fit["T1"][0]) / 4) * 4
-        plt.xlabel("Delay [ns]")
-        plt.ylabel("I quadrature [V]")
-        print(f"Qubit decay time to update in the config: qubit_T1 = {qubit_T1:.0f} ns")
-        plt.legend((f"Relaxation time T1 = {qubit_T1:.0f} ns",))
-        plt.title("T1 measurement")
-    except (Exception,):
-        pass
-
-    node.results['figure'] = fig
-    node.results['figure_fit'] = fig2
-
-    node.save()
+# %%
+node.results['initial_parameters'] = node.parameters.model_dump()
+node.machine = machine
+node.save()
+# %%
