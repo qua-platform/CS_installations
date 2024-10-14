e0cc44cbd2047235e2e6a4c9cd48f54b6f5a5a1b
HiroQM <hiroyuki.inoue@quantum-machines.co>
2024-10-14 21:48:24 +0900

    wip

--- configuration/make_wiring.py
+++ configuration/make_wiring.py
@@ -26,6 +26,18 @@ connectivity = Connectivity()
 connectivity.add_resonator_line(qubits=qubits)
 connectivity.add_qubit_drive_lines(qubits=qubits)
 
+qubit_pairs = [
+    (1, 2), (2, 1),
+    (2, 3), (3, 2),
+]
+control_qubits = [c for c, t in qubit_pairs] # get opx_iq_octave_spec from connectivity
+target_qubits = [t for c, t in qubit_pairs] # get opx_iq_octave_spec from connectivity
+control_qubit_constraints = len(control_qubits) * [opx_iq_octave_spec] # control_qubits
+target_qubit_constraints = len(target_qubits) * [opx_iq_octave_spec] # target_qubits
+for qp, cqc, tqc in zip(qubit_pairs, control_qubit_constraints, target_qubit_constraints):
+    connectivity.add_qubit_pair_control_lines(qubit_pairs=qp, constraints=cqc)
+    connectivity.add_qubit_pair_target_lines(qubit_pairs=qp, constraints=tqc)
+
 # Allocate the wiring to the connectivity object based on the available instruments
 allocate_wiring(connectivity, instruments)
 
