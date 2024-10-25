951c1dc5b44e8ea1f2c30399bccf7c166d139afc
paulQM <paul.jamet@quantum-machines.co>
2024-10-25 21:05:14 +0200

    config without octave

--- configuration.py
+++ configuration.py
@@ -1,9 +1,11 @@
 import numpy as np
+from qm import QuantumMachine
 from qualang_tools.plot import interrupt_on_close
 from qualang_tools.results import fetching_tool, progress_counter
 from qualang_tools.units import unit
 
-from qm import QuantumMachine
+from quam.quam.utils import pulse
+
 #######################
 # AUXILIARY FUNCTIONS #
 #######################
@@ -36,6 +38,7 @@ qop_port = None
 octave_config = None
 
 # Frequencies
+pulsed_laser_AOM_IF = 100 * u.MHz
 readout_AOM_IF = 100 * u.MHz
 control_AOM_IF = 100 * u.MHz
 control_EOM_IF = 100 * u.MHz
@@ -45,7 +48,24 @@ control_EOM_LO = 6 * u.GHz
 readout_aom_len = 100 * u.ns
 control_aom_len = 100 * u.ns
 control_eom_len = 100 * u.ns
+pulsed_laser_aom_len = 100 * u.ns
 
+# Delays
+readout_aom_delay = 0 * u.ns
+control_aom_delay = 0 * u.ns
+control_eom_delay = 0 * u.ns
+pulsed_laser_aom_delay = 0 * u.ns
+
+# Amplitudes
+readout_amp = 0.1
+control_aom_amp = 0.1
+control_eom_amp = 0.1
+pulsed_laser_amp = 0.1
+
+
+#################
+# CONFIGURATION #
+#################
 
 config = {
     "version": 1,
@@ -69,145 +89,105 @@ config = {
                 8: {},
                 9: {},
             },
-            "analog_inputs": {}
+            "analog_inputs": {},
         }
     },
     "elements": {
         "readout_aom": {
             "SingleInput": {
-                "port": ("con1", 1),
+                "port": ("con1", 1),  # TODO: assign the right port
+                "delay": readout_aom_delay,
             },
             "intermediate_frequency": readout_AOM_IF,
             "operations": {
-                "readout": "readout_pulse",
+                "readout": "cw_readout_aom",
             },
-        },
-        "AOM": {
             "digitalInputs": {
                 "marker": {
-                    "port": ("con1", 1),
-                    "delay": laser_delay,
-                    "buffer": 0,
+                    "port": ("con1", 1),  # TODO: what is this?
+                    "delay": readout_aom_delay,
                 },
-            },
-            "operations": {
-                "laser_ON": "laser_ON",
             },
         },
-        "A_transition": {
-            "singleInput": {"port": ("con1", 1)},
-            "intermediate_frequency": optical_transition_IF,
-            "digitalInputs": {
-                "marker0": {
-                    "port": ("con1", 1),
-                    "delay": laser_delay,
-                    "buffer": 0,
-                },
-                "marker1": {
-                    "port": ("con1", 2),
-                    "delay": laser_delay,
-                    "buffer": 0,
-                },
+        "control_aom": {
+            "SingleInput": {
+                "port": ("con1", 1),  # TODO: assign the right port
+                "delay": control_aom_delay,
             },
+            "intermediate_frequency": control_AOM_IF,
             "operations": {
-                "laser_ON": "laser_ON_A",
+                "control": "cw_control_aom",
             },
-        },
-        "B_RF": {
-            "singleInput": {"port": ("con1", 3)},
-            "intermediate_frequency": optical_transition_IF,
-            "operations": {
-                "+cw": "+const_pulse",
-                "-cw": "-const_pulse",
-            },
-        },
-        "F_transition": {
-            "digitalInputs": {
-                "marker0": {
-                    "port": ("con1", 3),
-                    "delay": laser_delay,
-                    "buffer": 0,
-                },
-                "marker1": {
-                    "port": ("con1", 4),
-                    "delay": laser_delay,
-                    "buffer": 0,
-                },
-            },
-            "operations": {
-                "laser_ON": "laser_ON",
-            },
-        },
-        "excited_state_mw": {
-            "digitalInputs": {
-                "switch0": {
-                    "port": ("con1", 5),
-                    "delay": laser_delay,
-                    "buffer": 0,
-                },
-                "switch1": {
-                    "port": ("con1", 6),
-                    "delay": laser_delay,
-                    "buffer": 0,
-                },
-            },
-            "operations": {
-                "switch_ON": "switch_ON",
-            },
-        },
-        "SNSPD": {
-            # "singleInput": {"port": ("con1", 1)},  # not used
-            "intermediate_frequency": Yb_IF_freq,
             "digitalInputs": {
                 "marker": {
-                    "port": ("con1", 2),
-                    "delay": detection_delay,
-                    "buffer": 0,
+                    "port": ("con1", 1),  # TODO: what is this?
+                    "delay": control_aom_delay,
                 },
             },
+        },
+        "control_eom": {
+            "SingleInput": {
+                "port": ("con1", 1),  # TODO: assign the right port
+                "delay": control_eom_delay,
+            },
+            "intermediate_frequency": control_EOM_IF,
             "operations": {
-                "readout": "readout_pulse",
-                "long_readout": "long_readout_pulse",
+                "control": "cw_control_eom",
             },
-            "outputs": {"out1": ("con1", 1)},
-            "outputPulseParameters": {
-                "signalThreshold": signal_threshold,
-                "signalPolarity": "Ascending",
-                "derivativeThreshold": 1023,
-                "derivativePolarity": "Below",
+            "digitalInputs": {
+                "marker": {
+                    "port": ("con1", 1),  # TODO: what is this?
+                    "delay": control_eom_delay,
+                },
             },
-            "time_of_flight": detection_delay,
-            "smearing": 0,
+        },
+        "pulsed_laser_aom": {
+            "SingleInput": {
+                "port": ("con1", 1),  # TODO: assign the right port
+                "delay": pulsed_laser_aom_delay,
+            },
+            "intermediate_frequency": pulsed_laser_AOM_IF,
+            "operations": {
+                "control": "cw_pulsed_laser_aom",
+            },
+            "digitalInputs": {
+                "marker": {
+                    "port": ("con1", 1),  # TODO: what is this?
+                    "delay": pulsed_laser_aom_delay,
+                },
+            },
         },
     },
     "pulses": {
-        "readout_pulse": {
+        "cw_readout_aom": {
+            "operation": "readout",
+            "length": readout_aom_len,
+            "waveforms": {"single": "cw_r"},
+        },
+        "cw_control_aom": {
             "operation": "control",
-            "length": mw_len_NV,
-            "waveforms": {"single": "cw_wf"},
+            "length": control_aom_len,
+            "waveforms": {"single": "cw_c_a"},
+        },
+        "cw_control_eom": {
+            "operation": "control",
+            "length": control_eom_len,
+            "waveforms": {"single": "cw_c_e"},
+        },
+        "cw_pulsed_laser_aom": {
+            "operation": "control",
+            "length": pulsed_laser_aom_len,
+            "waveforms": {"single": "cw_pl_a"},
         },
     },
     "waveforms": {
-        "cw_wf": {"type": "constant", "sample": mw_amp_NV},
-        "+cw_wf": {"type": "constant", "sample": B_RF_amp},
-        "-cw_wf": {"type": "constant", "sample": -B_RF_amp},
-        "pi_wf": {"type": "constant", "sample": pi_amp_NV},
-        "minus_pi_wf": {"type": "constant", "sample": -pi_amp_NV},
-        "pi_half_wf": {"type": "constant", "sample": pi_half_amp_NV},
-        "minus_pi_half_wf": {"type": "constant", "sample": -pi_half_amp_NV},
-        "zero_wf": {"type": "constant", "sample": 0.0},
+        "cw_r": {"type": "constant", "sample": readout_amp},
+        "cw_c_a": {"type": "constant", "sample": control_aom_amp},
+        "cw_c_e": {"type": "constant", "sample": control_eom_amp},
+        "cw_pl_a": {"type": "constant", "sample": pulsed_laser_amp},
     },
     "digital_waveforms": {
         "ON": {"samples": [(1, 0)]},  # [(on/off, ns)]
         "OFF": {"samples": [(0, 0)]},  # [(on/off, ns)]
-    },
-    "mixers": {
-        "mixer_Yb": [
-            {
-                "intermediate_frequency": Yb_IF_freq,
-                "lo_frequency": Yb_LO_freq,
-                "correction": IQ_imbalance(0.0, 0.0),
-            },
-        ],
     },
 }
