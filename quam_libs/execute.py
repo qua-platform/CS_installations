91f4781fe7967ab1fa9f3327e1aede11944e83b9
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-11-26 12:46:09 +0100

    update execute

--- quam_libs/execute.py
+++ quam_libs/execute.py
@@ -26,56 +26,55 @@ def get_job_results(job):
     return results
 
 
-# def generate_config(machine):
-#     # Generate the OPX and Octave configurations
-#     config = machine.generate_config()
-#     octaves_config = config.pop("octaves")
-#     for key, channel in config["elements"].items():
-#         if "RF_outputs" in channel:  # OPX input IQ channels
-#             RF_outputs_entry = channel.pop("RF_outputs")
-#             octave, idx = RF_outputs_entry["port"]
-#             if idx != 1:
-#                 raise ValueError("Only RF output 1 is supported for Octave")
+def strip_octaves_from_config(config):
 
-#             IF_outputs = octaves_config[octave]["IF_outputs"]
-#             channel["outputs"] = {
-#                 "out1": IF_outputs["IF_out1"]["port"],
-#                 "out2": IF_outputs["IF_out2"]["port"],
-#             }
-#         if "RF_inputs" in channel:  # OPX output IQ channels
-#             RF_inputs_entry = channel.pop("RF_inputs")
-#             octave, idx = RF_inputs_entry["port"]
-#             RF_input = octaves_config[octave]["RF_outputs"][idx]
+    octaves_config = config.pop("octaves")
+    for key, channel in config["elements"].items():
+        if "RF_outputs" in channel:  # OPX input IQ channels
+            RF_outputs_entry = channel.pop("RF_outputs")
+            octave, idx = RF_outputs_entry["port"]
+            if idx != 1:
+                raise ValueError("Only RF output 1 is supported for Octave")
 
-#             channel["mixInputs"] = {
-#                 "I": RF_input["I_connection"],
-#                 "Q": RF_input["Q_connection"],
-#                 "mixer": f"{key}.mixer",
-#                 "lo_frequency": RF_input["LO_frequency"],
-#             }
+            IF_outputs = octaves_config[octave]["IF_outputs"]
+            channel["outputs"] = {
+                "out1": IF_outputs["IF_out1"]["port"],
+                "out2": IF_outputs["IF_out2"]["port"],
+            }
+        if "RF_inputs" in channel:  # OPX output IQ channels
+            RF_inputs_entry = channel.pop("RF_inputs")
+            octave, idx = RF_inputs_entry["port"]
+            RF_input = octaves_config[octave]["RF_outputs"][idx]
 
-#             try:
-#                 q_name, ch_name = key.split(".")
-#                 qubit = machine.qubits[q_name]
-#                 ch = getattr(qubit, ch_name)
-#                 mixer_calibration = ch.mixer_calibration or [1, 0, 0, 1]
-#             except Exception:
-#                 mixer_calibration = [1, 0, 0, 1]
+            channel["mixInputs"] = {
+                "I": RF_input["I_connection"],
+                "Q": RF_input["Q_connection"],
+                "mixer": f"{key}.mixer",
+                "lo_frequency": RF_input["LO_frequency"],
+            }
 
-#             config["mixers"] = {
-#                 f"{key}.mixer": [
-#                     {
-#                         "intermediate_frequency": channel["intermediate_frequency"],
-#                         "lo_frequency": RF_input["LO_frequency"],
-#                         "correction": mixer_calibration,
-#                     }
-#                 ]
-#             }
+            try:
+                q_name, ch_name = key.split(".")
+                qubit = machine.qubits[q_name]
+                ch = getattr(qubit, ch_name)
+                mixer_calibration = ch.mixer_calibration or [1, 0, 0, 1]
+            except Exception:
+                mixer_calibration = [1, 0, 0, 1]
 
-#     return config
+            config["mixers"] = {
+                f"{key}.mixer": [
+                    {
+                        "intermediate_frequency": channel["intermediate_frequency"],
+                        "lo_frequency": RF_input["LO_frequency"],
+                        "correction": mixer_calibration,
+                    }
+                ]
+            }
+
+    return config
 
 
-def generate_config(machine: QuAM):
+def generate_config(machine: QuAM, strip_octaves: bool = True):
     # Generate the OPX and Octave configurations
     for label, qubit in machine.qubits.items():
         if label in machine.active_qubit_names:
@@ -83,6 +82,8 @@ def generate_config(machine: QuAM):
         else:
             qubit.z.opx_output.offset = qubit.z.min_offset
     config = machine.generate_config()
+    if strip_octaves:
+        config = strip_octaves_from_config(config)
     return config
 
 
@@ -94,6 +95,7 @@ def execute_program(
     *,
     token: Optional[str] = None,
     debug: bool = False,
+    strip_octaves: bool = True,
 ): ...
 
 
@@ -104,6 +106,7 @@ def execute_program(
     execution_mode: str = "local",
     *,
     close_other_machines: bool = True,
+    strip_octaves: bool = True,
 ): ...
 
 
@@ -115,6 +118,7 @@ def execute_program(
     token: Optional[str] = None,
     debug: bool = False,
     close_other_machines: bool = True,
+    strip_octaves: bool = True,
 ):
     if execution_mode.lower() == "iqcc":
         return execute_IQCC(program, machine, token=token, debug=debug)
@@ -124,9 +128,9 @@ def execute_program(
         )
 
 
-def execute_local(program, machine, close_other_machines=True):
+def execute_local(program, machine, close_other_machines=True, strip_octaves=True):
     qmm = machine.connect()
-    config = machine.generate_config()
+    config = generate_config(machine, strip_octaves=strip_octaves)
 
     # Open the quantum machine
     qm = qmm.open_qm(config, close_other_machines=close_other_machines)
@@ -142,7 +146,7 @@ def execute_local(program, machine, close_other_machines=True):
     return results
 
 
-def execute_IQCC(program, machine, token=None, debug=False):
+def execute_IQCC(program, machine, token=None, debug=False, strip_octaves=True):
     if token is None:
         for path in [
             Path(".IQCC_token"),
@@ -160,9 +164,9 @@ def execute_IQCC(program, machine, token=None, debug=False):
             )
     token = token.strip()
 
-    qc = IQCC_Cloud(token)
+    qc = IQCC_Cloud(api_token=token)
 
-    config = generate_config(machine)
+    config = generate_config(machine, strip_octaves=strip_octaves)
     results = qc.execute(program, config, True, debug=debug)
     return results["result"]
 
