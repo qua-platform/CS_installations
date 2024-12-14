c9f016ebe26b38c93aab4335a4bbc5f348d60427
HiroQM <hiroyuki.inoue@quantum-machines.co>
2024-12-14 14:17:20 +0000

    init

---
+++ 04_reflectometry_spectroscopy.py
@@ -0,0 +1,172 @@
+"""
+        RF REFLECTOMETRY SPECTROSCOPY
+The goal of this script is to perform the spectroscopy of the RF-reflectometry readout.
+For this, the frequency of the element (pulser) used for reflectometry readout is being swept and the signal reflected
+by the tank circuit is being acquired, demodulated and integrated by the OPX.
+
+A global averaging is performed (averaging on the most outer loop) and the data is extracted while the program is running
+to display the frequency response of the tank circuit with increasing SNR.
+
+Prerequisites:
+    - Connect the tank circuit to the corresponding output and input channels.
+
+Before proceeding to the next node:
+    - Update the config with the resonance frequency for reflectometry readout.
+"""
+
+from typing import List, Literal, Optional
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from qm import QuantumMachinesManager, SimulationConfig
+from qm.qua import *
+from qualang_tools.loops import from_array
+from qualang_tools.plot import interrupt_on_close
+from qualang_tools.results import fetching_tool, progress_counter
+from qualang_tools.units import unit
+
+# %% {Imports}
+from qualibrate import NodeParameters, QualibrationNode
+from scipy import signal
+
+from configuration_with_octave import *
+
+# from qua_config.configuration_with_octave import *
+
+
+# %% {Node_parameters}
+class Parameters(NodeParameters):
+    qubits: Optional[List[str]] = None
+    num_averages: int = 100
+    frequency_min_in_mhz: float = 50.0
+    frequency_max_in_mhz: float = 350.0
+    frequency_step_in_mhz: float = 0.1
+    simulate: bool = False
+    timeout: int = 100
+
+
+node = QualibrationNode(name="04_reflectometry_spectroscopy", parameters=Parameters())
+
+
+###################
+# The QUA program #
+###################
+n_avg = node.parameters.num_averages
+# The frequency sweep around the resonator resonance frequency
+freq_min = node.parameters.frequency_min_in_mhz * u.MHz
+freq_max = node.parameters.frequency_max_in_mhz * u.MHz
+step = node.parameters.frequency_step_in_mhz * u.MHz
+frequencies = np.arange(freq_min, freq_max + 1, step)
+
+with program() as reflectometry_spectro:
+    f = declare(int)  # QUA variable for the frequency sweep
+    n = declare(int)  # QUA variable for the averaging loop
+    I = declare(fixed)  # QUA variable for the measured 'I' quadrature
+    Q = declare(fixed)  # QUA variable for the measured 'Q' quadrature
+    I_st = declare_stream()  # Stream for the 'I' quadrature
+    Q_st = declare_stream()  # Stream for the 'Q' quadrature
+    n_st = declare_stream()  # Stream for the averaging iteration 'n'
+
+    with for_(n, 0, n < n_avg, n + 1):
+        with for_(*from_array(f, frequencies)):
+            # Update the frequency of the tank_circuit element
+            update_frequency("tank_circuit", f)
+            # RF reflectometry: the voltage measured by the analog input 2 is recorded, demodulated at the readout
+            # frequency and the integrated quadratures are stored in "I" and "Q"
+            # Please choose the right "out1" or "out2" according to the connectivity
+            measure(
+                "readout",
+                "tank_circuit",
+                None,
+                demod.full("cos", I, "out2"),
+                demod.full("sin", Q, "out2"),
+            )
+            save(I, I_st)
+            save(Q, Q_st)
+            # Wait at each iteration in order to ensure that the data will not be transferred faster than 1 sample
+            # per µs to the stream processing. Otherwise, the processor will receive the samples faster than it can
+            # process them which can cause the OPX to crash.
+            wait(1_000 * u.ns)  # in ns
+        save(n, n_st)
+
+    with stream_processing():
+        I_st.buffer(len(frequencies)).average().save("I")
+        Q_st.buffer(len(frequencies)).average().save("Q")
+        n_st.save("iteration")
+
+#####################################
+#  Open Communication with the QOP  #
+#####################################
+qmm = QuantumMachinesManager(
+    host=qop_ip, port=qop_port, cluster_name=cluster_name, octave=octave_config
+)
+
+#######################
+# Simulate or execute #
+#######################
+simulate = False
+save_data = True
+
+if simulate:
+    # Simulates the QUA program for the specified duration
+    simulation_config = SimulationConfig(duration=10_000)  # In clock cycles = 4ns
+    # Simulate blocks python until the simulation is done
+    job = qmm.simulate(config, reflectometry_spectro, simulation_config)
+    # Plot the simulated samples
+    job.get_simulated_samples().con1.plot()
+
+else:
+    # Open the quantum machine
+    qm = qmm.open_qm(config)
+    # Send the QUA program to the OPX, which compiles and executes it
+    job = qm.execute(reflectometry_spectro)
+    # Get results from QUA program
+    results = fetching_tool(job, data_list=["I", "Q", "iteration"], mode="live")
+    # Live plotting
+    fig = plt.figure()
+    interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
+    while results.is_processing():
+        # Fetch results
+        I, Q, iteration = results.fetch_all()
+        # Convert results into Volts
+        S = u.demod2volts(I + 1j * Q, reflectometry_readout_length)
+        R = np.abs(S)  # Amplitude
+        phase = np.angle(S)  # Phase
+        # Progress bar
+        progress_counter(iteration, n_avg, start_time=results.get_start_time())
+        # Plot results
+        plt.suptitle("RF-reflectometry spectroscopy")
+        plt.subplot(211)
+        plt.cla()
+        plt.plot(frequencies / u.MHz, R)
+        plt.xlabel("Readout frequency [MHz]")
+        plt.ylabel(r"$R=\sqrt{I^2 + Q^2}$ [V]")
+        plt.subplot(212)
+        plt.cla()
+        plt.plot(frequencies / u.MHz, signal.detrend(np.unwrap(phase)))
+        plt.xlabel("Readout frequency [MHz]")
+        plt.ylabel("Phase [rad]")
+        plt.tight_layout()
+        plt.pause(0.1)
+
+    if save_data:
+        from qualang_tools.results.data_handler import DataHandler
+
+        # Data to save
+        save_data_dict = {}
+        save_data_dict["I"] = I
+        save_data_dict["Q"] = Q
+
+        # Save results
+        script_name = Path(__file__).name
+        data_handler = DataHandler(root_data_folder=save_dir)
+        save_data_dict.update({"fig_live": fig})
+        data_handler.additional_files = {
+            script_name: script_name,
+            **default_additional_files,
+        }
+        data_handler.save_data(data=save_data_dict, name="04_reflectometry_spectroscopy")
+
+    plt.show()
+    qm.close()
