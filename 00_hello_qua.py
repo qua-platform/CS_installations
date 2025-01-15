647a4a0d4466e07acf7f43a7dff671109d516a6b
paulQM <paul.jamet@quantum-machines.co>
2025-01-15 11:33:07 +0100

    add scripts

---
+++ 00_hello_qua.py
@@ -0,0 +1,43 @@
+"""
+A simple sandbox to showcase different QUA functionalities during the installation.
+"""
+
+import matplotlib.pyplot as plt
+from configuration import *
+from qm import QuantumMachinesManager, SimulationConfig
+from qm.qua import *
+
+###################
+# The QUA program #
+###################
+with program() as hello_qua:
+    a = declare(fixed)
+    with infinite_loop_():
+        with for_(a, 0, a < 1.1, a + 0.05):
+            play("pi" * amp(a), "qubit")
+        wait(25, "qubit")
+
+#####################################
+#  Open Communication with the QOP  #
+#####################################
+qmm = QuantumMachinesManager(host=qop_ip, port=qop_port, cluster_name=cluster_name, octave=octave_config)
+
+###########################
+# Run or Simulate Program #
+###########################
+
+simulate = False
+
+if simulate:
+    # Simulates the QUA program for the specified duration
+    simulation_config = SimulationConfig(duration=2_000 >> 2)  # In clock cycles = 4ns
+    # Simulate blocks python until the simulation is done
+    job = qmm.simulate(config, hello_qua, simulation_config)
+    # Plot the simulated samples
+    job.get_simulated_samples().con1.plot()
+    plt.show()
+else:
+    # Open a quantum machine to execute the QUA program
+    qm = qmm.open_qm(config)
+    # Send the QUA program to the OPX, which compiles and executes it - Execute does not block python!
+    job = qm.execute(hello_qua)
---
+++ configuration.py
@@ -0,0 +1,203 @@
+"""
+QUA-Config supporting OPX+ & Octave
+"""
+
+from pathlib import Path
+
+import numpy as np
+from qm.octave import QmOctaveConfig
+from qualang_tools.config.waveform_tools import drag_gaussian_pulse_waveforms
+from qualang_tools.units import unit
+
+#######################
+# AUXILIARY FUNCTIONS #
+#######################
+u = unit(coerce_to_integer=True)
+
+
+######################
+# Network parameters #
+######################
+qop_ip = "172.16.33.101"  # Write the QM router IP address
+cluster_name = "Cluster_81"  # Write your cluster_name if version >= QOP220
+qop_port = None  # Write the QOP port if version < QOP220
+
+# Path to save data
+save_dir = Path().absolute() / "QM" / "INSTALLATION" / "data"
+
+########################
+# Octave configuration #
+#######################
+
+octave_config = QmOctaveConfig()
+octave_config.set_calibration_db(Path().absolute())
+
+#############################################
+#                  Qubits                   #
+#############################################
+qubit_LO = 4 * u.GHz
+qubit_IF = 50 * u.MHz
+
+
+# Continuous wave
+const_len = 100
+const_amp = 0.25
+# Saturation_pulse
+saturation_len = 10 * u.us
+saturation_amp = 0.1
+# Square pi pulse
+square_pi_len = 40
+square_pi_amp = 0.25
+
+
+#############################################
+#                Resonators                 #
+#############################################
+resonator_LO = 7 * u.GHz
+resonator_IF = 60 * u.MHz
+
+readout_len = 2000
+readout_amp = 0.1
+
+time_of_flight = 24
+
+
+#############################################
+#                  Config                   #
+#############################################
+config = {
+    "version": 1,
+    "controllers": {
+        "con1": {
+            "analog_outputs": {
+                1: {"offset": 0.0},  # I resonator
+                2: {"offset": 0.0},  # Q resonator
+                3: {"offset": 0.0},  # I qubit
+                4: {"offset": 0.0},  # Q qubit
+            },
+            "digital_outputs": {},
+            "analog_inputs": {
+                1: {"offset": 0.0, "gain_db": 0},  # I from down-conversion
+                2: {"offset": 0.0, "gain_db": 0},  # Q from down-conversion
+            },
+        }
+    },
+    "elements": {
+        "qubit": {
+            "RF_inputs": {"port": ("oct1", 2)},
+            "intermediate_frequency": qubit_IF,
+            "operations": {
+                "cw": "const_pulse",
+                "saturation": "saturation_pulse",
+                "pi": "square_pi_pulse",
+                "pi_half": "square_pi_half_pulse",
+            },
+        },
+        "resonator": {
+            "RF_inputs": {"port": ("oct1", 1)},
+            "RF_outputs": {"port": ("oct1", 1)},
+            "intermediate_frequency": resonator_IF,
+            "operations": {
+                "cw": "const_pulse",
+                "readout": "readout_pulse",
+            },
+            "time_of_flight": time_of_flight,
+            "smearing": 0,
+        },
+    },
+    "octaves": {
+        "oct1": {
+            "RF_outputs": {
+                1: {
+                    "LO_frequency": resonator_LO,
+                    "LO_source": "internal",
+                    "output_mode": "always_on",
+                    "gain": 0,
+                },
+                2: {
+                    "LO_frequency": qubit_LO,
+                    "LO_source": "internal",
+                    "output_mode": "always_on",
+                    "gain": 0,
+                },
+            },
+            "RF_inputs": {
+                1: {
+                    "LO_frequency": resonator_LO,
+                    "LO_source": "internal",
+                },
+            },
+            "connectivity": "con1",
+        }
+    },
+    "pulses": {
+        "const_pulse": {
+            "operation": "control",
+            "length": const_len,
+            "waveforms": {
+                "I": "const_wf",
+                "Q": "zero_wf",
+            },
+        },
+        "square_pi_pulse": {
+            "operation": "control",
+            "length": square_pi_len,
+            "waveforms": {
+                "I": "square_pi_wf",
+                "Q": "zero_wf",
+            },
+        },
+        "square_pi_half_pulse": {
+            "operation": "control",
+            "length": square_pi_len,
+            "waveforms": {
+                "I": "square_pi_half_wf",
+                "Q": "zero_wf",
+            },
+        },
+        "saturation_pulse": {
+            "operation": "control",
+            "length": saturation_len,
+            "waveforms": {"I": "saturation_drive_wf", "Q": "zero_wf"},
+        },
+        "readout_pulse": {
+            "operation": "measurement",
+            "length": readout_len,
+            "waveforms": {
+                "I": "readout_wf",
+                "Q": "zero_wf",
+            },
+            "integration_weights": {
+                "cos": "cosine_weights",
+                "sin": "sine_weights",
+                "minus_sin": "minus_sine_weights",
+            },
+            "digital_marker": "ON",
+        },
+    },
+    "waveforms": {
+        "const_wf": {"type": "constant", "sample": const_amp},
+        "saturation_drive_wf": {"type": "constant", "sample": saturation_amp},
+        "square_pi_wf": {"type": "constant", "sample": square_pi_amp},
+        "square_pi_half_wf": {"type": "constant", "sample": square_pi_amp / 2},
+        "zero_wf": {"type": "constant", "sample": 0.0},
+        "readout_wf": {"type": "constant", "sample": readout_amp},
+    },
+    "digital_waveforms": {
+        "ON": {"samples": [(1, 0)]},
+    },
+    "integration_weights": {
+        "cosine_weights": {
+            "cosine": [(1.0, readout_len)],
+            "sine": [(0.0, readout_len)],
+        },
+        "sine_weights": {
+            "cosine": [(0.0, readout_len)],
+            "sine": [(1.0, readout_len)],
+        },
+        "minus_sine_weights": {
+            "cosine": [(0.0, readout_len)],
+            "sine": [(-1.0, readout_len)],
+        },
+    },
+}
