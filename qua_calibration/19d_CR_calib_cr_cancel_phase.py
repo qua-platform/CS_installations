18905e506d52589f1d14c3c12efc9af406195145
HuanQM <huan.kung@quantum-machines.co>
2025-04-23 21:28:20 +0800

    check

--- qua_calibration/19d_CR_calib_cr_cancel_phase.py
+++ qua_calibration/19d_CR_calib_cr_cancel_phase.py
@@ -42,7 +42,7 @@ Reference: Sarah Sheldon, Easwar Magesan, Jerry M. Chow, and Jay M. Gambetta Phy
 
 from qm.qua import *
 from qm import QuantumMachinesManager
-from configuration import *
+from configuration_mw_fem import *
 import matplotlib.pyplot as plt
 from qm import SimulationConfig
 from qualang_tools.loops import from_array
