62015099abc06daf4116103558b58261aecb7dcc
HuanQM <huan.kung@quantum-machines.co>
2025-04-27 01:38:00 +0800

    Add

---
+++ qua_single_transmon/06a_qubit_spectroscopy.py
@@ -0,0 +1,168 @@
+"""
+        QUBIT SPECTROSCOPY
+This sequence involves sending a saturation pulse to the qubit, placing it in a mixed state,
+and then measuring the state of the resonator across various qubit drive intermediate dfs.
+In order to facilitate the qubit search, the qubit pulse duration and amplitude can be changed manually in the QUA
+program directly without having to modify the configuration.
+
+The data is post-processed to determine the qubit resonance frequency, which can then be used to adjust
+the qubit intermediate frequency in the configuration under "qubit_IF".
+
+Note that it can happen that the qubit is excited by the image sideband or LO leakage instead of the desired sideband.
+This is why calibrating the qubit mixer is highly recommended.
+
+This step can be repeated using the "x180" operation instead of "saturation" to adjust the pulse parameters (amplitude,
+duration, frequency) before performing the next calibration steps.
+
+Prerequisites:
+    - Identification of the resonator's resonance frequency when coupled to the qubit in question (referred to as "resonator_spectroscopy").
+    - Calibration of the IQ mixer connected to the qubit drive line (whether it's an external mixer or an Octave port).
+    - Configuration of the saturation pulse amplitude and duration to transition the qubit into a mixed state.
+    - Specification of the expected qubit T1 in the configuration.
+
+Before proceeding to the next node:
+    - Update the qubit frequency, labeled as "qubit_IF", in the configuration.
+"""
+
+from qm.qua import *
+from qm import QuantumMachinesManager
+from qm import SimulationConfig
+from configuration import *
+from qualang_tools.results import progress_counter, fetching_tool
+from qualang_tools.plot import interrupt_on_close
+from qualang_tools.loops import from_array
+import matplotlib.pyplot as plt
+from qualang_tools.results.data_handler import DataHandler
+
+#   Parameters   #
+##################
+# Parameters Definition
+n_avg = 10000  # The number of averages
+# Adjust the pulse duration and amplitude to drive the qubit into a mixed state
+saturation_len = 10 * u.us  # In ns
+saturation_amp = 0.5  # pre-factor to the value defined in the config - restricted to [-2; 2)
+# Qubit detuning sweep
+center = 0 * u.MHz
+span = 10 * u.MHz
+df = 100 * u.kHz
+dfs = np.arange(-span, +span + 0.1, df)
+
+# Data to save
+save_data_dict = {
+    "n_avg": n_avg,
+    "frequencies": dfs,
+    "saturation_len": saturation_len,
+    "saturation_amp": saturation_amp,
+    "config": config,
+}
+
+###################
+# The QUA program #
+###################
+with program() as qubit_spec:
+    n = declare(int)  # QUA variable for the averaging loop
+    df = declare(int)  # QUA variable for the qubit frequency
+    I = declare(fixed)  # QUA variable for the measured 'I' quadrature
+    Q = declare(fixed)  # QUA variable for the measured 'Q' quadrature
+    I_st = declare_stream()  # Stream for the 'I' quadrature
+    Q_st = declare_stream()  # Stream for the 'Q' quadrature
+    n_st = declare_stream()  # Stream for the averaging iteration 'n'
+
+    with for_(n, 0, n < n_avg, n + 1):
+        with for_(*from_array(df, dfs)):
+            # Update the frequency of the digital oscillator linked to the qubit element
+            update_frequency("qubit", df + center)
+            # Play the saturation pulse to put the qubit in a mixed state - Can adjust the amplitude on the fly [-2; 2)
+            play("saturation" * amp(saturation_amp), "qubit", duration=saturation_len * u.ns)
+            # Align the two elements to measure after playing the qubit pulse.
+            # One can also measure the resonator while driving the qubit by commenting the 'align'
+            align("qubit", "resonator")
+            # Measure the state of the resonator
+            measure(
+                "readout",
+                "resonator",
+                None,
+                dual_demod.full("cos", "sin", I),
+                dual_demod.full("minus_sin", "cos", Q),
+            )
+            # Wait for the qubit to decay to the ground state
+            wait(thermalization_time * u.ns, "resonator")
+            # Save the 'I' & 'Q' quadratures to their respective streams
+            save(I, I_st)
+            save(Q, Q_st)
+        # Save the averaging iteration to get the progress bar
+        save(n, n_st)
+
+    with stream_processing():
+        # Cast the data into a 1D vector, average the 1D vectors together and store the results on the OPX processor
+        I_st.buffer(len(dfs)).average().save("I")
+        Q_st.buffer(len(dfs)).average().save("Q")
+        n_st.save("iteration")
+
+#####################################
+#  Open Communication with the QOP  #
+#####################################
+qmm = QuantumMachinesManager(host=qop_ip, port=qop_port, cluster_name=cluster_name, octave=octave_config)
+
+###########################
+# Run or Simulate Program #
+###########################
+simulate = False
+
+if simulate:
+    # Simulates the QUA program for the specified duration
+    simulation_config = SimulationConfig(duration=10_000)  # In clock cycles = 4ns
+    # Simulate blocks python until the simulation is done
+    job = qmm.simulate(config, qubit_spec, simulation_config)
+    # Get the simulated samples
+    samples = job.get_simulated_samples()
+    # Plot the simulated samples
+    samples.con1.plot()
+    # Get the waveform report object
+    waveform_report = job.get_simulated_waveform_report()
+    # Cast the waveform report to a python dictionary
+    waveform_dict = waveform_report.to_dict()
+    # Visualize and save the waveform report
+    waveform_report.create_plot(samples, plot=True, save_path=str(Path(__file__).resolve()))
+
+else:
+    # Open the quantum machine
+    qm = qmm.open_qm(config)
+    # Send the QUA program to the OPX, which compiles and executes it
+    job = qm.execute(qubit_spec)
+    # Get results from QUA program
+    results = fetching_tool(job, data_list=["I", "Q", "iteration"], mode="live")
+    # Live plotting
+    fig = plt.figure()
+    interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
+    while results.is_processing():
+        # Fetch results
+        I, Q, iteration = results.fetch_all()
+        # Convert results into Volts
+        S = u.demod2volts(I + 1j * Q, readout_len)
+        R = np.abs(S)  # Amplitude
+        phase = np.angle(S)  # Phase
+        # Progress bar
+        progress_counter(iteration, n_avg, start_time=results.get_start_time())
+        # Plot results
+        plt.suptitle(f"Qubit spectroscopy - LO = {qubit_LO / u.GHz} GHz")
+        plt.subplot(211)
+        plt.cla()
+        plt.plot((dfs + center) / u.MHz, R, ".")
+        plt.xlabel("Qubit intermediate frequency [MHz]")
+        plt.ylabel(r"$R=\sqrt{I^2 + Q^2}$ [V]")
+        plt.subplot(212)
+        plt.cla()
+        plt.plot((dfs + center) / u.MHz, phase, ".")
+        plt.xlabel("Qubit intermediate frequency [MHz]")
+        plt.ylabel("Phase [rad]")
+        plt.pause(0.1)
+        plt.tight_layout()
+    # Save results
+    script_name = Path(__file__).name
+    data_handler = DataHandler(root_data_folder=save_dir)
+    save_data_dict.update({"I_data": I})
+    save_data_dict.update({"Q_data": Q})
+    save_data_dict.update({"fig_live": fig})
+    data_handler.additional_files = {script_name: script_name, **default_additional_files}
+    data_handler.save_data(data=save_data_dict, name="_".join(script_name.split("_")[1:]).split(".")[0])
