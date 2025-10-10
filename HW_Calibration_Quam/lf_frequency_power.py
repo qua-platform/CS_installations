1ca187dee4e2ae0989e665f7e7b1cfc916b9fe3f
KU-QM <kalid.ulas@quantum-machines.co>
2025-10-10 17:01:40 +0100

    ruff

--- HW_Calibration_Quam/lf_frequency_power.py
+++ HW_Calibration_Quam/lf_frequency_power.py
@@ -1,7 +1,6 @@
 import sys
-sys.path.append(
-    "/Users/kalidu_laptop/QUA/CS_installations/HW_Calibration_Quam"
-)
+
+sys.path.append("/Users/kalidu_laptop/QUA/CS_installations/HW_Calibration_Quam")
 from QUAM_setup import BaseQuam
 from qm.qua import *
 import numpy as np
@@ -11,62 +10,50 @@ from qualang_tools.units import unit
 u = unit(coerce_to_integer=True)
 
 
-
 def QM_lf_freq_power_setup(
     QM_machine: BaseQuam,
-    QM_output: int, 
-    output_mode: str = "direct", 
-    frequency: float = 31.25e6
+    QM_output: int,
+    output_mode: str = "direct",
+    frequency: float = 31.25e6,
 ) -> None:
-    
     QM_machine.mw_channels = {}
     QM_machine.dc_channels = {}
 
-    if output_mode == "direct": 
+    if output_mode == "direct":
         maxamp = 0.5
     elif output_mode == "amplified":
         maxamp = 2.5
-    else: 
-        raise ValueError(f"output_mode not valid. Received {output_mode}, not in ['direct', 'amplified]")
+    else:
+        raise ValueError(
+            f"output_mode not valid. Received {output_mode}, not in ['direct', 'amplified]"
+        )
 
     QM_machine.add_dc_channel(
-        port_id = QM_output, 
-        intermediate_frequency = frequency, 
-        output_mode = output_mode
+        port_id=QM_output, intermediate_frequency=frequency, output_mode=output_mode
     )
 
     QM_machine.add_dc_flux_pulse(
-        port_id = QM_output, 
-        pulse_name = "const_pulse", 
-        duration = 10000, 
-        amplitude = maxamp
+        port_id=QM_output, pulse_name="const_pulse", duration=10000, amplitude=maxamp
     )
     dc_ch = QM_machine.dc_channels[str(1)]
 
-    with program() as QM_machine.qua_program: 
+    with program() as QM_machine.qua_program:
         with infinite_loop_():
-            dc_ch.play(
-                "const_pulse"
-            )
+            dc_ch.play("const_pulse")
 
 
 machine = BaseQuam()
-machine.connect(host = "172.16.33.115", cluster_name = "CS_4")
+machine.connect(host="172.16.33.115", cluster_name="CS_4")
 machine.lf_fem = 5
 
 test_frequencies = [31.25e6, 62.25e6, 125e6, 250e6]
 frequency = test_frequencies[0]
 
 QM_lf_freq_power_setup(
-    QM_machine = machine, 
-    QM_output = 1, 
-    output_mode = "direct", 
-    frequency = frequency
+    QM_machine=machine, QM_output=1, output_mode="direct", frequency=frequency
 )
 
 # results = machine.open_new_QM_and_execute(fetch_results = False)
-waveform_report = machine.open_new_QM_and_simulate(duration = 1000)
+waveform_report = machine.open_new_QM_and_simulate(duration=1000)
 
 # machine.halt_running_jobs()
-
-
--- HW_Calibration_Quam/lf_rise_time.py
+++ HW_Calibration_Quam/lf_rise_time.py
@@ -1,7 +1,6 @@
 import sys
-sys.path.append(
-    "/Users/kalidu_laptop/QUA/CS_installations/HW_Calibration_Quam"
-)
+
+sys.path.append("/Users/kalidu_laptop/QUA/CS_installations/HW_Calibration_Quam")
 from QUAM_setup import BaseQuam
 from qm.qua import *
 import numpy as np
@@ -13,60 +12,46 @@ u = unit(coerce_to_integer=True)
 
 def QM_lf_rise_time(
     QM_machine: BaseQuam,
-    QM_output: int, 
-    output_mode: str = "direct", 
-    square_wave_frequency: float = 31.25e6
+    QM_output: int,
+    output_mode: str = "direct",
+    square_wave_frequency: float = 31.25e6,
 ) -> None:
-    
     QM_machine.mw_channels = {}
     QM_machine.dc_channels = {}
 
-    if output_mode == "direct": 
+    if output_mode == "direct":
         maxamp = 0.5
     elif output_mode == "amplified":
         maxamp = 2.5
-    else: 
-        raise ValueError(f"output_mode not valid. Received {output_mode}, not in ['direct', 'amplified]")
+    else:
+        raise ValueError(
+            f"output_mode not valid. Received {output_mode}, not in ['direct', 'amplified]"
+        )
 
     QM_machine.add_dc_channel(
-        port_id = QM_output, 
-        intermediate_frequency = 0, 
-        output_mode = output_mode
+        port_id=QM_output, intermediate_frequency=0, output_mode=output_mode
     )
 
-    duration = int((1/square_wave_frequency)/2 * 1e9)
+    duration = int((1 / square_wave_frequency) / 2 * 1e9)
 
     QM_machine.add_dc_flux_pulse(
-        port_id = QM_output, 
-        pulse_name = "const_pulse", 
-        duration = duration, 
-        amplitude = maxamp
+        port_id=QM_output, pulse_name="const_pulse", duration=duration, amplitude=maxamp
     )
     dc_ch = QM_machine.dc_channels[str(1)]
 
-    with program() as QM_machine.qua_program: 
+    with program() as QM_machine.qua_program:
         with infinite_loop_():
-            dc_ch.play(
-                "const_pulse"
-            )
-            dc_ch.play(
-                "const_pulse", amplitude_scale=-1
-            )
+            dc_ch.play("const_pulse")
+            dc_ch.play("const_pulse", amplitude_scale=-1)
 
 
 machine = BaseQuam()
-machine.connect(host = "172.16.33.115", cluster_name = "CS_4")
+machine.connect(host="172.16.33.115", cluster_name="CS_4")
 machine.lf_fem = 5
 
-QM_lf_rise_time(
-    QM_machine = machine, 
-    QM_output = 1, 
-    output_mode = "amplified"
-)
+QM_lf_rise_time(QM_machine=machine, QM_output=1, output_mode="amplified")
 
 # results = machine.open_new_QM_and_execute(fetch_results = False)
-waveform_report = machine.open_new_QM_and_simulate(duration = 1000)
+waveform_report = machine.open_new_QM_and_simulate(duration=1000)
 
 # machine.halt_running_jobs()
-
-
